d6d8abf0ca9942752e65fe101ec967cdb84f6ce4
WARDOG080 <kawasakidog080@gmail.com>
2024-01-27 20:10:56 +0900

    second commit

--- routes/hello.js
+++ routes/hello.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', finction(req, res, next) {
-  res.render('hello', { title: 'Hello Express' }); 
+router.get('/', function(req, res, next) {
+  res.render('hello', { title: 'Hello Express' });
 });
 
 module.exports = router;
